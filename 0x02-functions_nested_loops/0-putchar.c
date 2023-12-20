adb891c7c3503dc0c2678b4bbf50f30a8be29b70
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-19 18:36:36 -0800

    pushing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program outputs _putchar'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+}
+
