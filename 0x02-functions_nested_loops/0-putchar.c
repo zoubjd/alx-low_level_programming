0ebc2fc7e68a3c259a3b77fe0a01a8e25e2d067c
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-20 17:42:46 -0800

    hoppefully the last time pushing this task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,7 @@
 
 int main(void)
 {
-/*
-char str[] = "_putchar";
-int i = 0;
-while (i < 9)
-{
-_putchar(str[i]);
-i++;
-}
-*/
-write(1, "_putchar\n", 10);
+write(1, "_putchar\n", 9);
 return (0);
 }
 
