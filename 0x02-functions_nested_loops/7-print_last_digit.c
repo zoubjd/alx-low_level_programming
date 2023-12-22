f9642303007eb877d13f64ccd62bea641428e5ad
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-21 17:47:00 -0800

    jj

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
-    if (last_digit < 0)
-    {
-        last_digit = -last_digit;
-    }
-    _putchar(last_digit + '0');
-    return last_digit;
+int last_digit = n % 10;
+if (last_digit < 0)
+{
+last_digit = -last_digit;
+}
+_putchar(last_digit + '0');
+return (last_digit);
 }
 
