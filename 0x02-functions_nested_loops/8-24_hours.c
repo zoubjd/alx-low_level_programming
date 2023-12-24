7533aa6cd2408ac867bc79c32e7313e5c369b6e0
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-23 18:33:47 -0800

    hopes all good

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
-#include <stddef.h>
 #include "main.h"
+
 /**
  * jack_bauer - Entry point
  * Description: 'the program outputs every hour and minute of the day'
@@ -10,32 +10,20 @@ void jack_bauer(void)
 {
 int hours = 0;
 int minutes = 0;
-write(1, "00:00", 5);
 while (hours < 24)
 {
 while (minutes < 60)
 {
-if (hours < 10)
-{
-write(1, "0", 1);
-write(1, &hours, sizeof(hours));
-}
-else
-{
-write(1, &hours, sizeof(hours));
-}
-write(1, ":", 1);
-if (minutes < 10)
-{
-write(1, "0", 1);
-write(1, &minutes, sizeof(minutes));
-}
-else
-{
-write(1, &minutes, sizeof(minutes));
-}
+_putchar('0' + (hours / 10));
+_putchar('0' + (hours % 10));
+_putchar(':');
+_putchar('0' + (minutes / 10));
+_putchar('0' + (minutes % 10));
+_putchar('\n');
 minutes++;
 }
+minutes = 0;
 hours++;
 }
 }
+
