43a34bedb75ae17816ddaa0c742acd2b74895023
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-24 18:55:40 -0800

    first try? again

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ * Description: 'the program outputs the number from n to 98'
+ * @n: is the number to count from
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i == 98)
+{
+printf(" %d", i);
+}
+else
+{
+printf(" %d,", i);
+}
+}
+}
+else if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+if (i == 98)
+{
+printf(" %d", i);
+}
+else
+{
+printf(" %d,", i);
+}
+}
+}
+else
+{
+printf(" %d", 98);
+}
+}
+
