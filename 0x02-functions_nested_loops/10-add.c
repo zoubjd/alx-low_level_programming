babc820e39a17b55b6937f4c57f550d06c8f4d31
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-24 18:38:58 -0800

    first try?

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: 'the program outputs the addition of two ints'
+ * @m: is the first int
+ * @n: is the second int
+ * Return: Always 0 (Success)
+ */
+int add(int m, int n)
+{
+int result = 0;
+result = m + n;
+return (result);
+}
+
