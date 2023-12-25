8a03e2fc8078376fff5736d149d24b8dc6f57798
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-24 18:13:40 -0800

    :)))

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - Entry point
+ * Description: 'the program outputs the multiple tables up to 9'
+ * (Success)
+ */
+void times_table(void)
+{
+int i, j;
+int result;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+result = i * j;
+if (result < 10)
+{
+_putchar('0' + result);
+}
+else
+{
+_putchar('0' + (result / 10));
+_putchar('0' + (result % 10));
+}
+_putchar(',');
+}
+_putchar('\n');
+}
+}
+
