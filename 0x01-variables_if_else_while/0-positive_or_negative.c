71c0f4f9c42e1fbb3265b43b1eb15a3ada85ebc5
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-25 17:24:19 -0800

    jj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
-int main(void) /* this is the main function that checks the number*/
+/**
+ * main - Entry point
+ * Description: 'the program outputs the sign of n'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int n;
 
