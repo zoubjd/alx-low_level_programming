fab2b811e6fca4f91e1168a2c523088600a36766
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-26 16:38:49 -0800

    last push

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'the program outputs the whole alphabets'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar('0' + i);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
