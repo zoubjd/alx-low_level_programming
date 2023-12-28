8952251d173f343598bd2d9bead7e1e8d85e08f3
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-27 16:40:26 -0800

    trying

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -9,7 +9,15 @@ int main(void)
 {
 int i;
 i = 0;
-positive_or_negative(i);
+char test[] = positive_or_negative(i);
+char result[] = "0 is zero";
+if (strcmp(test, result) == 0)
+{
 return (0);
+}
+else
+{
+return (1);
+}
 }
 
