84f697631bb4583d637e182ae23eb6a31a894f8a
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-29 17:32:53 -0800

    final push

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * Description: 'the program outputs a number of /'
+ * @n: is the num of /s
+ * Return: (Success)
+ */
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('/');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
+
