8492205500a0a8e73068074f163302833eeb5fea
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-29 16:41:54 -0800

    thirs task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * mul - Entry point
+ * Description: 'the program outputs multiplication of anb'
+ * @a: is the first int that's multipled
+ * @b: is the second int that's multipled
+ * Return: (Success)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
