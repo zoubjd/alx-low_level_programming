6f31f0a476c8d24b8853c5bd196e439ef2f53bac
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-29 16:34:13 -0800

    second task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * Description: 'the program outputs if c is number'
+ * @c: is the ccharacter that's checked
+ * Return: (Success)
+ */
+
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
