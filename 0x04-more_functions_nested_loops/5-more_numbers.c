bdcb70d8c643c282ff9aed1079118ccc24e77b68
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-29 17:03:27 -0800

    sixth task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Description: 'the program outputs the first 15 nums'
+ * Return: (Success)
+ */
+
+void more_numbers(void)
+{
+int i;
+for (i = 0; i < 15; i++)
+{
+if (i > 9 && (i / 10) > 0)
+{
+_putchar('0' + (i / 10));
+}
+_putchar('0' + (i % 10));
+}
+_putchar('\n');
+}
+
