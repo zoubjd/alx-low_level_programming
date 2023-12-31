8efa6d0b6d0a6f01794e2784a098e10f742bb4e9
zoubjd <zouhairbajdouri19@gmail.com>
2023-12-30 18:21:20 -0800

    gonna sleep rq

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_triangle - Entry point
+ * Description: 'the program outputs a number of tringles'
+ * @size: is the height
+ * Return: (Success)
+ */
+
+void print_triangle(int size)
+{
+int i = 0;
+if (size > 0)
+{
+while (i < size)
+{
+int j = 0;
+int y = 0;
+while (j < (size - i - 1))
+{
+_putchar(' ');
+j++;
+}
+while (y <= i)
+{
+_putchar(35);
+y++;
+}
+_putchar('\n');
+i++;
+}
+}
+else 
+{
+_putchar('\n');
+}
+}
+
