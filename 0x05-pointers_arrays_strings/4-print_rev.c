4280cf189e4f799d7bcae10a31bf9a5b494f6722
zoubjd <zouhairbajdouri19@gmail.com>
2024-01-02 18:10:38 -0800

    nearly asleep

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * description: the program prints rev s
+ * @s: the string
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int count = 0;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+while (count >= 0)
+{
+_putchar(*s);
+count--;
+s--;
+}
+_putchar('\n');
+}
+
