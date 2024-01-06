fd807a3cde8f0f0fff2965bccaa9a8cdf5e795ff
zoubjd <zouhairbajdouri19@gmail.com>
2024-01-05 17:11:20 -0800

    damn

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
- * _strcat - check the code
- * description: the program merges 2 strings
+ * _strncat - check the code
+ * description: the program gothers 2 strings
  * @dest: is the first string
  * @src: is the second string
+ * @n: is the num of byte to take from src
  * Return: Always 0.
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
 char *start = dest;
 while (*dest)
 {
 dest++;
 }
-while (*src)
+while (*src && i < n)
 {
 *dest = *src;
 dest++;
 src++;
+i++;
 }
 *dest = '\0';
 return (start);
