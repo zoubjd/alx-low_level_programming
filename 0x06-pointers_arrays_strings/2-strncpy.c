27c5792e8f92dbf73857188b0987f3e8b4646ef8
zoubjd <zouhairbajdouri19@gmail.com>
2024-01-05 17:19:55 -0800

    i think it missed a line

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * p_strncpy - check the code
+ * description: the program copies a string to another
+ * @dest: is the first string
+ * @src: is the second string
+ * @n: is the num of bytes
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+char *start = dest;
+while (*src && i < n)
+{
+*dest = *src;
+dest++;
+src++;
+i++;
+}
+*dest = '\0';
+return (start);
+}
