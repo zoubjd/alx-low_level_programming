fd957ffdb8f42906b90fc9af24407b6516c96de4
zoubjd <zouhairbajdouri19@gmail.com>
2024-01-09 07:48:29 -0800

    should i add a new line?

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
+/*
+ * main - description
+ * program: outputs a string
+ * return 0
+ * */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
