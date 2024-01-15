3e6eb1b240e6e66b18f33b3189644ee4e2e9f2ed
zoubjd <zouhairbajdouri19@gmail.com>
2024-01-14 17:16:39 -0800

    that shits lagging

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * description: the program prints its name
+ * @argc: the array
+ * @argv: the content
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
