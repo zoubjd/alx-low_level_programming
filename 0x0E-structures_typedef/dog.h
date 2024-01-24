16e25264fdc81912b0e519b83514073a3d2a19d1
zoubjd <zouhairbajdouri19@gmail.com>
2024-01-24 21:59:24 +0100

    bad betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Short description
+ * @name: the name of the doc
+ * @age: the age of the dog
+ * @owner: The name of the owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
