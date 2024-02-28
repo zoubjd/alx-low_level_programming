42fb0eb02ff50e838667732c7cb29fa1f4795495
zoubjd <zouhairbajdouri19@gmail.com>
2024-02-28 18:07:24 +0100

    pp

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,26 +4,29 @@
  * read_textfile - read text from file
  * @filename: name of file
  * @letters: number of bytes
- * 
  * Return: number of bytes
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes;
-    char *buf;
+int fd;
+ssize_t bytes;
+char *buf;
 
-    if (!filename || !letters)
-        return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-    buf = malloc(sizeof(char) * letters);
-    bytes = read(fd, &buf[0], letters);
-    bytes = write(STDOUT_FILENO, &buf[0], bytes);
-    free(buf);
-    close(fd);
-    return (bytes);
+if (!filename || !letters)
+{
+return (0);
+}
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+return (0);
+}
+buf = malloc(sizeof(char) * letters);
+bytes = read(fd, &buf[0], letters);
+bytes = write(STDOUT_FILENO, &buf[0], bytes);
+free(buf);
+close(fd);
+return (bytes);
 }
 
